9e8199e90d8275f5d5d19ef80cc79d3280c78d67
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-10-30 08:19:05 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # play
-sudo apt install wget i3status htop hardinfo neofetch xorg vlc pulseaudio volumeicon-alsa  pcmanfm terminator nitrogen compton picom lxappearance alsa-utils pavucontrol  unzip  geeqie arandr libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi arc-theme libpam0g-dev libxcb-xkb-dev
+sudo apt install wget i3status htop hardinfo neofetch xorg vlc pulseaudio volumeicon-alsa  pcmanfm terminator nitrogen compton picom lxappearance alsa-utils pavucontrol  unzip  geeqie arandr arc-theme 
 git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
 cd Top-5-Bootloader-Themes
 chmod +x install.sh
