59f5108818cc6d1d94c88953fe2f4cdea178c54d
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-10-30 08:33:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
-# play
-sudo apt install wget i3status htop hardinfo neofetch xorg vlc pulseaudio volumeicon-alsa  pcmanfm terminator nitrogen compton picom lxappearance alsa-utils pavucontrol  unzip  geeqie arandr arc-theme 
-git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
-cd Top-5-Bootloader-Themes
-chmod +x install.sh
-sudo ./install.sh
-printf "\e[1;32mYou can now reboot! Thanks talha.\e[0m\n"
+sudo apt install i3-wm wget i3status htop hardinfo neofetch xorg vlc pulseaudio volumeicon-alsa pcmanfm terminator nitrogen compton picom lxappearance alsa-utils pavucontrol unzip geeqie arandr arc-theme 
+          
+
 
 
 
