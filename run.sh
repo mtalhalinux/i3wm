49c7feee316ca1eea481c549c0305919fef308a5
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-10-30 08:39:58 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+sudo apt install i3-wm wget i3status htop hardinfo neofetch xorg vlc pulseaudio volumeicon-alsa pcmanfm terminator nitrogen compton picom lxappearance alsa-utils pavucontrol unzip geeqie arandr arc-theme 
+          
+
+
+
+
