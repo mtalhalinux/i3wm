ef40c51e7d990cb34214a49041e43f56ed7fc105
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-10-31 00:39:35 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
-sudo apt install i3-wm i3status xorg vlc pulseaudio volumeicon-alsa pcmanfm terminator feh alsa-utils pavucontrol dmenu
+sudo apt install i3status xorg vlc pulseaudio volumeicon-alsa pcmanfm terminator feh alsa-utils pavucontrol dmenu
 sudo apt install wget htop hardinfo neofetch compton picom lxappearance      
 sudo apt install unzip geeqie arandr arc-theme network-manager-gnome
 sudo apt install libpam0g-dev libxcb-xkb-dev
 sudo apt install ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
 sudo apt install build-essential dkms linux-headers-$(uname -r)
+sudo apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+git clone https://github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+mkdir -p build && cd build
+meson --prefix /usr/local
+ninja
+sudo ninja install
+cd ../
 git clone --recurse-submodules https://github.com/fairyglade/ly
 cd ly
 make
