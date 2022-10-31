4339a6a9da0c6c8e0e8e73a817d86d47ffd4fb2f
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-10-30 19:38:09 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 sudo apt install i3-wm i3status xorg vlc pulseaudio volumeicon-alsa pcmanfm terminator nitrogen alsa-utils pavucontrol  
 sudo apt install wget htop hardinfo neofetch compton picom lxappearance      
 sudo apt install unzip geeqie arandr arc-theme
-su
-apt install build-essential libpam0g-dev libxcb-xkb-dev
-exit
+sudo apt install build-essential libpam0g-dev libxcb-xkb-dev
 git clone --recurse-submodules https://github.com/fairyglade/ly
 cd ly
 make
