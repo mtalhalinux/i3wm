0c175315cfd20e4f9d03d93aebe054289c4eca44
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-10-30 19:32:31 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 sudo apt install i3-wm i3status xorg vlc pulseaudio volumeicon-alsa pcmanfm terminator nitrogen alsa-utils pavucontrol  
 sudo apt install wget htop hardinfo neofetch compton picom lxappearance      
 sudo apt install unzip geeqie arandr arc-theme
+su
+apt install build-essential libpam0g-dev libxcb-xkb-dev
+exit
+git clone --recurse-submodules https://github.com/fairyglade/ly
+cd ly
+make
+make run
+make install installsystemd
+systemctl enable ly.service
+
 
 
 
